595347707132087b6928640e27ac507a19ce8f18
Dachrono <andres_zamorano_@hotmail.com>
2023-10-18 13:10:26 -0600

    Solve some identation issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  
-  validates :name, presence: true
 
+  validates :name, presence: true
 end
