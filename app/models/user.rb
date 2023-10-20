e4b2eea42005bee827ea9cdca9871848921df40c
Dachrono <andres_zamorano_@hotmail.com>
2023-10-19 21:37:59 -0600

    Add relations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  has_many :categories
+  has_many :payments       
+
   validates :name, presence: true
 end
